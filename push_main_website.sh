5839571df9ee33bc2a5df53c41ef9fa6b2a30ef0
T. Latrille <thibault.latrille@ens-lyon.org>
2026-03-09 09:52:01 +0100

    Update deploy strategy

--- push_main_website.sh
+++ push_main_website.sh
@@ -5,14 +5,10 @@ set -euo pipefail
 git checkout main
 git push origin main
 
-git checkout website
-git rebase main
-git checkout main -- .
-git reset HEAD
-
 source .venv/bin/activate
 python -m scripts.generate
 
+git checkout -B website
 git add --force static/ index.html MonPotager.html
 git commit -m "Generate static site"
 git push origin website --force
