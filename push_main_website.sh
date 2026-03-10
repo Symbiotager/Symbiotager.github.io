8dec6b66cd3fdd028ab080ac65bbfa189f5daa31
T. Latrille <thibault.latrille@ens-lyon.org>
2026-03-10 13:57:28 +0100

    Added all html files

--- push_main_website.sh
+++ push_main_website.sh
@@ -11,7 +11,7 @@ python -m scripts.merge_data
 python -m scripts.generate
 
 git checkout -B website
-git add --force static/ index.html MonPotager.html merged.html
+git add --force static/ *.html
 git commit -m "Generate static site"
 git push origin website --force
 
